d2e88d78be52b96736fb7da3d81f5c51257534dc
Robert Sundström <robert.sundstrom@outlook.com>
2022-01-01 23:38:49 +0100

    Extract config

---
+++ CatalogTest/Data/Configurations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using CatalogTest.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogTest.Configurations;
+
+public class ProductGroupConfiguration : IEntityTypeConfiguration<ProductGroup>
+{
+    public void Configure(EntityTypeBuilder<ProductGroup> builder)
+    {
+        builder.ToTable("ProductGroups", t => t.IsTemporal());
+        //builder.HasQueryFilter(i => i.Deleted == null);
+    }
+}
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products", t => t.IsTemporal());
+
+        builder
+            .HasMany(p => p.Options)
+            .WithMany(p => p.Products)
+            .UsingEntity<ProductOption>();
+    }
+}
+
+public class ProductOptionConfiguration : IEntityTypeConfiguration<ProductOption>
+{
+    public void Configure(EntityTypeBuilder<ProductOption> builder)
+    {
+        builder.ToTable("ProductOptions", t => t.IsTemporal());
+    }
+}
+
+public class OptionGroupConfiguration : IEntityTypeConfiguration<OptionGroup>
+{
+    public void Configure(EntityTypeBuilder<OptionGroup> builder)
+    {
+        builder.ToTable("OptionGroups", t => t.IsTemporal());
+    }
+}
+
+public class OptionConfiguration : IEntityTypeConfiguration<Option>
+{
+    public void Configure(EntityTypeBuilder<Option> builder)
+    {
+        builder.ToTable("Options", t => t.IsTemporal());
+    }
+}
+
+public class OptionValueConfiguration : IEntityTypeConfiguration<OptionValue>
+{
+    public void Configure(EntityTypeBuilder<OptionValue> builder)
+    {
+        builder.ToTable("OptionValues", t => t.IsTemporal());
+    }
+}
+
+public class ProductVariantConfiguration : IEntityTypeConfiguration<ProductVariant>
+{
+    public void Configure(EntityTypeBuilder<ProductVariant> builder)
+    {
+        builder.ToTable("ProductVariants", t => t.IsTemporal());
+    }
+}
+
+public class VariantValueConfiguration : IEntityTypeConfiguration<VariantValue>
+{
+    public void Configure(EntityTypeBuilder<VariantValue> builder)
+    {
+        builder.ToTable("VariantValues", t => t.IsTemporal());
+    }
+}
